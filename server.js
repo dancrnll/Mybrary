9fb367daa8330e4a1468cc2f3b74ce1192248255
Dan Cornell <dancrnll@yahoo.com>
2022-05-10 22:02:55 -0700

    added useNewUrlParser: true, useUnifiedTopology: true to db connect

--- server.js
+++ server.js
@@ -19,10 +19,15 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
 // mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on('error', error => console.error(error));
-db.once('open', error => console.log('Connected to Mongoose'));
+//mongoose.connect(process.env.DATABASE_URL);
+mongoose
+.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Database Connected'))
+.catch(err => console.log(err));
+
+// const db = mongoose.connection;
+// db.on('error', error => console.error(error));
+// db.once('open', error => console.log('Connected to Mongoose'));
 
 
 app.use('/', indexRouter);
